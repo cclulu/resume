306f487d7178123b9ccc6244f1d4ff3eef5eac9f
cclulu <cclulu11@gmail.com>
2020-06-11 16:07:44 -0700

    update home page

--- src/Component/header/header.jsx
+++ src/Component/header/header.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import styles from './header.module.css'
 import linkdinLogo from '../../media/images/LI-In-Bug.png'
-import githubLogo from '../../media/images/GitHub-Mark-64px.png'
+import githubLogo from '../../media/images/GitHub-Mark-Light-64px.png'
 
 class Header extends React.Component {
   render() {
     return (
       <div className={styles.content}>
-        <h3 className={styles.headerThree}>Cuahuctemoc L. Osorio</h3>
+        <h1 className={styles.myName}>Cuahuctemoc L. Osorio</h1>
         <div className={styles.orgIcons}>
-          <a href="https://www.linkedin.com/in/cuahuctemoclo"> <img className={styles.linkdInIcon} src={linkdinLogo} alt="linkdinIcon"/></a>
           <a href="https://github.com/cclulu"> <img className={styles.githubIcon} src={githubLogo} alt="githubIcon"/></a>
+          <a href="https://www.linkedin.com/in/cuahuctemoclo"> <img className={styles.linkdInIcon} src={linkdinLogo} alt="linkdinIcon"/></a>
         </div>
       </div>
     )
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import Skills from '../../Component/skills/skills.jsx';
 import Cards from '../../Component/cards/cards.jsx';
+import styles from './home.module.css';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <p>FullStack developer, specializing in Java and Go.
-      I am passionate about automation and building or improving systems and infrastructure
-      using best practices to consistently deliver a better product</p>
-        <Skills/>
-        <Cards/>
+        <div className={styles.Content}>
+          <div className={styles.aboutMe}>
+            <h3> I am a FullStack developer, specializing in Java and Go, who is passionate about automation
+            and building or improving systems and infrastructure using best practices to consistently deliver a better product.
+            I have been a professional Software Developer since June of 2016</h3>
+          </div>
+          <Cards/>
+          <div className={styles.profilePic}></div>
+        </div>
       </div>
     )
   }
